349b84a9a9a813ebe13519513cc1a352107af965
Angel Scutari <antonioscutari2@hotmail.com>
2023-09-11 17:49:00 -0300

    mod fixed CORS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Header } from '@nestjs/common';
 import { AppService } from './app.service';
+
+const frontURL = process.env.FRONT_URL ?? 'http://localhost:5173';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get(':owner/:repo')
+  @Header('Access-Control-Allow-Origin', frontURL)
   getCommitHistory(@Param('owner') owner: string, @Param('repo') repo: string) {
     return this.appService.getCommitHistory(owner, repo);
   }
