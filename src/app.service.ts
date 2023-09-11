baeb88b9f984eb2817b9ce30080829d09e547779
Angel Scutari <antonioscutari2@hotmail.com>
2023-09-11 01:55:14 -0300

    add commitHistory service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { IAllCommits } from './icommits.interface';
+import { ICommit } from './icommit.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getCommitHistory(owner: string, repo: string): Promise<IAllCommits> {
+    const response = await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/commits`,
+    );
+    const data = await response.json();
+
+    const justRelevantData = data.map((commit: ICommit) => ({
+      sha: commit.sha,
+      node_id: commit.node_id,
+      commit: {
+        author: {
+          name: commit.commit.author.name,
+          email: commit.commit.author.email,
+          date: commit.commit.author.date,
+        },
+        committer: {
+          name: commit.commit.committer.name,
+          email: commit.commit.committer.email,
+          date: commit.commit.committer.date,
+        },
+        message: commit.commit.message,
+        tree: {
+          sha: commit.commit.tree.sha,
+          url: commit.commit.tree.url,
+        },
+        url: commit.commit.url,
+        comment_count: commit.commit.comment_count,
+      },
+      url: commit.url,
+      html_url: commit.html_url,
+      comments_url: commit.comments_url,
+      author: {
+        login: commit.author.login,
+        id: commit.author.id,
+        node_id: commit.author.node_id,
+        avatar_url: commit.author.avatar_url,
+        gravatar_id: commit.author.gravatar_id,
+        url: commit.author.url,
+        html_url: commit.author.html_url,
+      },
+      committer: {
+        login: commit.committer.login,
+        id: commit.committer.id,
+        node_id: commit.committer.node_id,
+        avatar_url: commit.committer.avatar_url,
+        gravatar_id: commit.committer.gravatar_id,
+        url: commit.committer.url,
+        html_url: commit.committer.html_url,
+      },
+    }));
+    return justRelevantData;
   }
 }
